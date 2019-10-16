61c5ec6d66d4f6ad9850e7471ce4494508cfadd5
danielsimonney <daniel.simonney60@gmail.com>
2019-10-16 12:21:55 +0200

    comments

--- src/Arena/RobotPosition.php
+++ src/Arena/RobotPosition.php
@@ -1,66 +1,78 @@
 <?php
 namespace Arena;
 
-class RobotPosition{
-  private $x;
-  private $y;
-  private $direction;
+class RobotPosition
+{
+    private $x;
+    private $y;
+    private $direction;
 
-  public function __construct($x, $y, $direction){
-    $this->x = $x;
-    $this->y = $y;
-    $this->direction = $direction;
-  }
-
-  public function getX(){
-    return $this->x;
-  }
-  public function getY(){
-    return $this->y;
-  }
-
-  public function getDirection(){
-    return $this->direction;
-  }
-
-  public function setDirection($direction){
-    $this->direction=$direction;
-  }
-
-  public function rotate($orientation){
-    if($orientation=="left"){
-      switch($this->direction){
-        case "N":
-        $this->setDirection("O");
-        break;
-        case "O":
-        $this->setDirection("S");
-        break;
-        case "S":
-        $this->setDirection("E");
-        break;
-        case "E":
-        $this->setDirection("N");
-        break; 
-      }
+    public function __construct($x, $y, $direction)
+    {
+        $this->x = $x;
+        $this->y = $y;
+        $this->direction = $direction;
     }
-    if($orientation=="right"){
-      switch($this->direction){
-        case "N":
-        $this->setDirection("E");
-        break;
-        case "E":
-        $this->setDirection("S");
-        break;
-        case "S":
-        $this->setDirection("O");
-        break;
-        case "O":
-        $this->setDirection("N");
-        break; 
-      }
+
+    public function getX()
+    {
+        return $this->x;
+    }
+    public function getY()
+    {
+        return $this->y;
+    }
+
+    public function getDirection()
+    {
+        return $this->direction;
     }
 
     
-  }
+    // Setter pour la direction du robot
+
+    public function setDirection($direction)
+    {
+        $this->direction = $direction;
+    }
+
+
+    // Change la direction du robot en fonction de sa direction actuelle et de l'orientation dans laquelle il se tourne 
+    
+    public function rotate($orientation)
+    {
+        if ($orientation == "left") {
+            switch ($this->direction) {
+                case "N":
+                    $this->setDirection("O");
+                    break;
+                case "O":
+                    $this->setDirection("S");
+                    break;
+                case "S":
+                    $this->setDirection("E");
+                    break;
+                case "E":
+                    $this->setDirection("N");
+                    break;
+            }
+        }
+        if ($orientation == "right") {
+            switch ($this->direction) {
+                case "N":
+                    $this->setDirection("E");
+                    break;
+                case "E":
+                    $this->setDirection("S");
+                    break;
+                case "S":
+                    $this->setDirection("O");
+                    break;
+                case "O":
+                    $this->setDirection("N");
+                    break;
+            }
+        }
+
+    }
 }
