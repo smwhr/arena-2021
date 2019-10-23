7dc966a6d3996e60dbe56958cd1eef3e0745dc5a
valugit <44845228+valugit@users.noreply.github.com>
2019-10-23 11:35:42 +0200

    Some changes :)

---
+++ src/Robot/ValuRobot.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Robot;
+
+class ValuRobot implements \Arena\Robot {
+  public function setSurroundings($position, $surroundings) {
+    //TODO
+  }
+
+  public function requestMove() {
+    //TODO
+  }
+}
