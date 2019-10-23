81f8f599d2f9773dd2532e470713817a8f07a8ce
TyomaMtl <scollioudfree.fr@gmail.com>
2019-10-23 13:04:26 +0200

    fix controller

--- src/controller.php
+++ src/controller.php
@@ -2,38 +2,6 @@
 require("../vendor/autoload.php");
 // require('../public/index.php');
 
-<<<<<<< HEAD
-if(isset($_POST['RESET']) || isset($_GET['RESET']))
-{
-	unset($_SESSION['arena']);
-	header('Location: '.$_SERVER['REQUEST_URI']);
-	exit;
-}
-
-if(!isset($_SESSION['arena']))
-{	
-	$classA = "Robot\\".($_POST['class_A'] ?? "DefaultRobot");
-	$classB = "Robot\\".($_POST['class_B'] ?? "DefaultRobot");
-	
-	$robotA = new $classA();
-	$robotB = new $classB();
-	$arena = new Arena\Arena($ascii_board, [$robotA, $robotB]);
-}
-else
-{
-	$arena = unserialize($_SESSION['arena']);
-}
-
-// try{
-	$turn_report = $arena->turn();
-	// }catch(Arena\WinningCondition $wc){
-		// $turn_report[] = $wc->getMessage();
-		// }
-		
-		
-		//don't forget to save to session the new state
-		$_SESSION['arena'] = serialize($arena);
-=======
 if(isset($_POST['restart'])){
   unset($_SESSION['arena']);
 } 
@@ -81,4 +49,3 @@ try{
 
 //don't forget to save to session the new state
 $_SESSION['arena'] = serialize($arena);
->>>>>>> 9489d9790d61698f44c8d8d039ed0590903e28cf
